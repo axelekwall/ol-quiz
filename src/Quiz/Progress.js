be8b0cd411a45e2ed3384a004b89beee01fab1a1
runhem <you@example.com>
2017-10-20 14:41:46 +0200

    Testing prog-bar

--- src/Quiz/Progress.js
+++ src/Quiz/Progress.js
@@ -2,12 +2,32 @@ import React, {Component} from 'react';
 
 
 class Progress extends Component {
+	constructor(props) {
+		console.log(props.progressCount)
+		super(props);
+		this.state = {
+         style  : {
+         	width : this.props.progressCount + 100 + 'px',
+            height : '50px',
+            backgroundColor : 'black',
+        }
+      };
+   }
+   setProgression() {
+    var style = {};
+    style.width = this.props.progressCount;
+}
 
   render() {
-  	return(
-  		<p>My progress is {this.props.progressCount}</p>
-  	)
+  	return(	
+  		<div className="prog-bar" style={{display:'none'}}>
+             <p  style={{display:'none'}} >My progress is {this.props.progressCount}</p>
+             <div className="prog-bar-fill" width={this.props.progressCount} style={{display:'none'}}></div>
+  		</div>
+
+  	);
   }
 }
 
 export default Progress;
+
