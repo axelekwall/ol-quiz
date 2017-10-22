182d1a89779de164d585d3b0a9de85e0a0977959
jthorel <jthorel@kth.se>
2017-10-22 18:35:26 +0200

    starting redux

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import store from './store';
+
+store.dispatch({type:"ANSWER"});
+store.dispatch({type:"ANSWER"});
 
 ReactDOM.render(<App/>, document.getElementById('root'));
 registerServiceWorker();
