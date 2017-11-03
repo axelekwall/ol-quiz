0c1816a55bfdb9735ae4f2ce357551f50948ccab
josmol <josmol@kth.se>
2017-11-03 22:15:07 +0100

    animation shit

--- src/Quiz/Answer.js
+++ src/Quiz/Answer.js
@@ -1,5 +1,22 @@
 import React, {Component} from 'react';
 
+import { tada, shake } from 'react-animations'
+import Radium, {StyleRoot} from 'radium';
+
+//Style för animation
+//Välj animationer här och byt ut: http://react-animations.herokuapp.com/
+const styles = {
+  tada: {
+    animation: 'x 1s',
+    animationName: Radium.keyframes(tada, 'tada')
+  },
+  shake: {
+    animation: 'x 1s',
+    animationName: Radium.keyframes(shake, 'shake')
+  }
+}
+
+
 class Answer extends Component {
   constructor(props){
     super(props)
@@ -33,21 +50,29 @@ class Answer extends Component {
 
   render() {
     var result;
+    var styleAnimation;
+
     if(this.props.selectedAnswer === this.props.question.correctIndex){
       result = "Rätt"
+      styleAnimation = styles.tada
       // ACTION - Set correct answer - dispatch
     }
     else{
       result = "Fel"
+      styleAnimation = styles.shake
     }
   	return(
   			<div className="answer-content">
-                <div className="answer-text">
+                <StyleRoot>
+                <div className="answer-text" style={styleAnimation}>
                     <h1>{result}</h1>
                 </div>
+                </StyleRoot>
+                
                 <div className="answer-statistics">
                     <h3>{this.state.percent}%</h3>
                 </div>
+                
                 <div className="answer-statistics-text">
                     <h4>har svarat rätt på den här frågan.</h4>
                 </div>
@@ -58,6 +83,7 @@ class Answer extends Component {
                     <button type="button" className="next-question-button" onClick={this.props.onClick}>Nästa</button>
                 </div>
 			</div>
+      
   	)
   }
 }
