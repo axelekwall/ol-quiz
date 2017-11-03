326f6f32b98351a57892f09d01324a382fa5aad4
josmol <josmol@kth.se>
2017-11-03 23:12:06 +0100

    remove react-motion import

--- src/Quiz/FrontView.js
+++ src/Quiz/FrontView.js
@@ -1,5 +1,8 @@
 import React, {Component} from 'react';
-import {Motion, spring} from 'react-motion';
+
+import { slideInLeft, fadeIn } from 'react-animations'
+import Radium, {StyleRoot} from 'radium';
+
 
 //Component includes 
 //* name of quiz
@@ -8,15 +11,40 @@ import {Motion, spring} from 'react-motion';
 
 //When clicking start user start quiz and is being directed to first question view
 
+// 
+const styles = {
+  slideInLeft: {
+    animation: 'x 1s',
+    animationName: Radium.keyframes(slideInLeft, 'slideInLeft')
+  },
+  fadeIn: {
+    animation: 'x 4s',
+    animationName: Radium.keyframes(fadeIn, 'fadeIn')
+  }
+}
+
+const SlowerSlideInStyle = {
+  slideInLeft: {
+    animation: 'x 1.5s',
+    animationName: Radium.keyframes(slideInLeft, 'slideInLeft')
+  }
+}
+
 const FrontView = ({quiz, onClick}) => (
   <div className="front-view-container">
 	 <div className="front-view-content">
-      <h1>{quiz.name}</h1>
-	    	<p>{quiz.desc}
+      <StyleRoot>
+      <h1 style={styles.slideInLeft}>{quiz.name}</h1>
+      </StyleRoot>
+      <StyleRoot>
+	    	<p style={SlowerSlideInStyle.slideInLeft}>{quiz.desc}
         <br/>
         <br/>
         Ta Osqledarens quiz och testa dina kunskaper!</p>
-        <button className="start-button"onClick={e => {onClick()}}>Starta quiz</button>
+      </StyleRoot>
+      <StyleRoot>
+        <button style={styles.fadeIn} className="start-button"onClick={e => {onClick()}}>Starta quiz</button>
+        </StyleRoot>
      </div>
   </div>
 )
