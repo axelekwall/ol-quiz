f060db4b7fc975e4b38e042bf516cdf62117c81c
Axel Ekwall <axel@minimize.se>
2017-11-07 23:00:45 +0100

    Added express to functions

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,35 @@
 const functions = require('firebase-functions');
+const express = require('express');
+const admin = require('firebase-admin');
+const quiz = require('./quiz-data.js');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+// Init Firebase Admin
+admin.initializeApp(functions.config().firebase);
+
+// Create Express app
+const app = express();
+
+// Setup Firestore references
+
+// Endpoints
+// Post answer to one question
+app.post('/api/answer', (req, res) => {
+  res.send('Hi from API!');
+});
+
+// Get result for one question
+app.get('/api/result/question/:questionId', (req, res) => {
+  const questionId = req.params['questionId'];
+});
+
+// Get quiz
+app.get('/api/quiz/:name', (req, res) => {
+  res.send('Hej');
+});
+
+// Get tota result fr one quiz
+app.get('/api/result/quiz/:quiz', (req, res) => {
+  res.send('Hej');
+});
+
+exports.app = functions.https.onRequest(app);
