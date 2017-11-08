307ce810599519aea3718c562cd6fa5cb0b10ce6
Axel Ekwall <axel@minimize.se>
2017-11-08 11:29:51 +0100

    Express cloud founction now works at /api

--- functions/index.js
+++ functions/index.js
@@ -1,35 +1,41 @@
 const functions = require('firebase-functions');
 const express = require('express');
 const admin = require('firebase-admin');
-const quiz = require('./quiz-data.js');
+// const quiz = require('./quiz-data.js');
 
 // Init Firebase Admin
 admin.initializeApp(functions.config().firebase);
 
 // Create Express app
 const app = express();
+const router = express.Router();
 
 // Setup Firestore references
 
 // Endpoints
 // Post answer to one question
-app.post('/api/answer', (req, res) => {
+router.post('/answer', (req, res) => {
   res.send('Hi from API!');
 });
 
 // Get result for one question
-app.get('/api/result/question/:questionId', (req, res) => {
+router.get('/result/question/:questionId', (req, res) => {
   const questionId = req.params['questionId'];
 });
 
 // Get quiz
-app.get('/api/quiz/:name', (req, res) => {
+router.get('/quiz/:name', (req, res) => {
+  res.send('Hej');
+});
+
+router.get('/', (req, res) => {
   res.send('Hej');
 });
 
 // Get tota result fr one quiz
-app.get('/api/result/quiz/:quiz', (req, res) => {
+router.get('/result/quiz/:quiz', (req, res) => {
   res.send('Hej');
 });
 
-exports.app = functions.https.onRequest(app);
+app.use('/api', router);
+exports.api = functions.https.onRequest(app);
