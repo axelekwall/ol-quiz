b4298621d9c5706bd7fe02d72948b5fc2b36d7b5
Frida <friekl@kth.se>
2017-11-08 15:19:21 +0100

    Finished page

--- src/Quiz/Finished.js
+++ src/Quiz/Finished.js
@@ -1,38 +1,61 @@
 import React, {Component} from 'react';
 
+var greeting = "Hurra!";
+var infotext = "Du är klar med quizet"
+
 class Finished extends Component {
 	constructor(props){
 		super(props)
-   	 	this.state = {percent: 0}
+   	this.state = {number: 0}
     this.tick = this.tick.bind(this)
     this.interval = 10;
+    this.endNumber = 6;
   }
 
   componentDidMount() {
       this.setState({percent: 0 });
 
       this.intervalRun = setInterval(this.tick, this.interval);
-    }
+  
+      if(this.props.correctAnswers >= 4){
+        greeting = "Grymt jobbat!"
+        infotext = "Du behöver inte oroa dig för något, du har koll på allt du behöver."
+      }else if(this.props.correctAnswers > 2){
+        greeting = "Det där gick ju helt okej!"
+        infotext = "Du har ju ganska bra koll ändå."
+      }else{
+        greeting = "Inte så bra jobbat!"
+        infotext = "Du kanske ska läsa på lite..."
+      }
+  }
 
   tick(){
-    if(this.state.percent < this.endPercent){
+    if(this.state.number < this.endNumber){
         clearInterval(this.intervalRun);
 
         this.interval = this.interval * 1.03;
 
         this.intervalRun = setInterval(this.tick, this.interval);
 
-        this.setState({percent: this.state.percent + 1});
+        this.setState({number: this.state.number + 1});
       }else{
         clearInterval(this.intervalRun);
       }
   }
-	render(){
+	
+  // this.state.number <- tickfunktion
+
+  render(){
 		return(
 			<div id="finished-view">
-    			<h1>Great job!</h1>
-    			<h2>{this.props.correctAnswers}/{this.state.number} correct answers!</h2>
-  			</div>
+    			<h2>Du fick rätt på {this.props.correctAnswers}/6 frågor</h2>
+          <h1>{greeting}</h1>
+          <p>{infotext}</p>
+    	
+          <div id="link-to-ol">
+          <a href="http://www.osqledaren.se" >Till Osqledaren.se</a>
+          </div>		
+  		</div>
 		)
 	}
 }
