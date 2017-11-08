550d25c4e01798cb1b4c7503189da32e95bbbeb9
Axel Ekwall <axel@minimize.se>
2017-11-08 16:02:08 +0100

    Api endpoint get quiz works

--- functions/index.js
+++ functions/index.js
@@ -1,41 +1,64 @@
 const functions = require('firebase-functions');
 const express = require('express');
 const admin = require('firebase-admin');
-// const quiz = require('./quiz-data.js');
 
 // Init Firebase Admin
 admin.initializeApp(functions.config().firebase);
 
 // Create Express app
 const app = express();
-const router = express.Router();
 
 // Setup Firestore references
+const db = admin.firestore();
 
-// Endpoints
+// Router setup
+const router = express.Router();
+
 // Post answer to one question
-router.post('/answer', (req, res) => {
+router.post('/result', (req, res) => {
   res.send('Hi from API!');
 });
 
 // Get result for one question
-router.get('/result/question/:questionId', (req, res) => {
-  const questionId = req.params['questionId'];
+router.get('/result/question/:id', (req, res) => {
+  const questionId = req.params['id'];
 });
 
 // Get quiz
 router.get('/quiz/:name', (req, res) => {
-  res.send('Hej');
-});
+  const quizName = req.params['name'];
+  let result = {};
+  const queries = [
+    db.collection('quiz').doc(quizName).get(),
+    db.collection('quiz').doc(quizName).collection('questions').get(),
+  ];
 
-router.get('/', (req, res) => {
-  res.send('Hej');
+  Promise.all(queries)
+  .then((data) => {
+    const quiz = data[0];
+    const questions = data[1];
+    const quizData = quiz.data();
+    result.id = quiz.id;
+    result.name = quizData.name;
+    result.desc = quizData.description;
+    result.questions = [];
+    questions.forEach((question) => {
+      result.questions.push({id: question.id, data: question.data()});
+    });
+    res.send(result);
+  })
+  .catch((error) => {
+    res.status(503).send(error);
+  });
 });
 
 // Get tota result fr one quiz
-router.get('/result/quiz/:quiz', (req, res) => {
+router.get('/result/quiz/:id', (req, res) => {
   res.send('Hej');
 });
 
+// Tell app about router...
 app.use('/api', router);
+
+// Export Express app as function "api"
 exports.api = functions.https.onRequest(app);
